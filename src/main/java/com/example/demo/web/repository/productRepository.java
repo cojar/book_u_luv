704a00806b18381312b425f54f155afd7a84ff24
Kael0595 <ants7021@naver.com>
2023-08-02 10:19:56 +0900

    fin

--- src/main/java/com/example/demo/web/repository/productRepository.java
+++ src/main/java/com/example/demo/web/repository/productRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.web.repository;
 
-public class productRepository {
+import com.example.demo.web.dto.productDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface productRepository extends JpaRepository<productDto, Long> {
+
 }
