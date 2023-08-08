b9544f9ac3708b3d83abaea2c124db0c5ea0a2e4
Kael0595 <ants7021@naver.com>
2023-08-08 12:31:06 +0900

    회원가입, 마이페이지 세팅

--- src/main/java/com/project/bookuluv/app/article/dto/ProductDto.java
+++
@@ -1,9 +0,0 @@
-package com.project.bookuluv.app.article.dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ProductDto {
-}
--- src/main/java/com/project/bookuluv/app/article/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.bookuluv.app.article.repository;
-
-import com.project.bookuluv.app.article.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
