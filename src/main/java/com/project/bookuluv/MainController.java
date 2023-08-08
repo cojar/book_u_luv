ba7ee0a247c817478bf95bb5eebc0bfa6f06c2ef
Kael0595 <ants7021@naver.com>
2023-08-08 13:10:01 +0900

    메인페이지 url 수정

--- src/main/java/com/project/bookuluv/MainController.java
+++ src/main/java/com/project/bookuluv/MainController.java
@@ -12,8 +12,4 @@ public class MainController {
         return "main"; // ROOT로 접근했을 때 페이지가 해당 주소로 리다이렉트 되게끔 리턴.
     }
 
-    @GetMapping("/main")
-    public String mainPage() {
-        return "main_page";
-    }
 }
